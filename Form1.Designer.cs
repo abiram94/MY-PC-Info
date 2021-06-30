5720a7918034f1607bbad7524377d6ace05b8ded
Abiram Karuppasamy <58244518+abiram94@users.noreply.github.com>
2021-06-30 15:55:13 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Mypc_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblcompname = new System.Windows.Forms.Label();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblipaddress = new System.Windows.Forms.Label();
+            this.lblmac = new System.Windows.Forms.Label();
+            this.lblsno = new System.Windows.Forms.Label();
+            this.lblmotherno = new System.Windows.Forms.Label();
+            this.lblramsize = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblofficeversion = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Computer Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label3.Location = new System.Drawing.Point(12, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MAC Address :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label4.Location = new System.Drawing.Point(12, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Username :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Motherboard No. :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label6.Location = new System.Drawing.Point(12, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "OS Version :";
+            // 
+            // lblcompname
+            // 
+            this.lblcompname.AutoSize = true;
+            this.lblcompname.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcompname.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblcompname.Location = new System.Drawing.Point(145, 12);
+            this.lblcompname.Name = "lblcompname";
+            this.lblcompname.Size = new System.Drawing.Size(46, 16);
+            this.lblcompname.TabIndex = 6;
+            this.lblcompname.Text = "label7";
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblusername.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblusername.Location = new System.Drawing.Point(145, 44);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(46, 16);
+            this.lblusername.TabIndex = 7;
+            this.lblusername.Text = "label8";
+            // 
+            // lblipaddress
+            // 
+            this.lblipaddress.AutoSize = true;
+            this.lblipaddress.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblipaddress.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblipaddress.Location = new System.Drawing.Point(145, 76);
+            this.lblipaddress.Name = "lblipaddress";
+            this.lblipaddress.Size = new System.Drawing.Size(46, 16);
+            this.lblipaddress.TabIndex = 8;
+            this.lblipaddress.Text = "label9";
+            // 
+            // lblmac
+            // 
+            this.lblmac.AutoSize = true;
+            this.lblmac.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmac.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblmac.Location = new System.Drawing.Point(145, 108);
+            this.lblmac.Name = "lblmac";
+            this.lblmac.Size = new System.Drawing.Size(54, 16);
+            this.lblmac.TabIndex = 9;
+            this.lblmac.Text = "label10";
+            // 
+            // lblsno
+            // 
+            this.lblsno.AutoSize = true;
+            this.lblsno.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsno.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblsno.Location = new System.Drawing.Point(145, 204);
+            this.lblsno.Name = "lblsno";
+            this.lblsno.Size = new System.Drawing.Size(54, 16);
+            this.lblsno.TabIndex = 10;
+            this.lblsno.Text = "label11";
+            // 
+            // lblmotherno
+            // 
+            this.lblmotherno.AutoSize = true;
+            this.lblmotherno.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmotherno.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblmotherno.Location = new System.Drawing.Point(145, 140);
+            this.lblmotherno.Name = "lblmotherno";
+            this.lblmotherno.Size = new System.Drawing.Size(54, 16);
+            this.lblmotherno.TabIndex = 11;
+            this.lblmotherno.Text = "label12";
+            // 
+            // lblramsize
+            // 
+            this.lblramsize.AutoSize = true;
+            this.lblramsize.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblramsize.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblramsize.Location = new System.Drawing.Point(145, 172);
+            this.lblramsize.Name = "lblramsize";
+            this.lblramsize.Size = new System.Drawing.Size(54, 16);
+            this.lblramsize.TabIndex = 13;
+            this.lblramsize.Text = "label12";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label8.Location = new System.Drawing.Point(12, 172);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "RAM Size  :";
+            // 
+            // lblofficeversion
+            // 
+            this.lblofficeversion.AutoSize = true;
+            this.lblofficeversion.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblofficeversion.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblofficeversion.Location = new System.Drawing.Point(145, 235);
+            this.lblofficeversion.Name = "lblofficeversion";
+            this.lblofficeversion.Size = new System.Drawing.Size(54, 16);
+            this.lblofficeversion.TabIndex = 15;
+            this.lblofficeversion.Text = "label11";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label9.Location = new System.Drawing.Point(13, 235);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Office Version :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 294);
+            this.Controls.Add(this.lblofficeversion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblramsize);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblmotherno);
+            this.Controls.Add(this.lblsno);
+            this.Controls.Add(this.lblmac);
+            this.Controls.Add(this.lblipaddress);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.lblcompname);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyPC_Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblcompname;
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.Label lblipaddress;
+        private System.Windows.Forms.Label lblmac;
+        private System.Windows.Forms.Label lblsno;
+        private System.Windows.Forms.Label lblmotherno;
+        private System.Windows.Forms.Label lblramsize;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblofficeversion;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
+using System.Management;
+
+namespace Mypc_Info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblcompname.Text = System.Environment.GetEnvironmentVariable("COMPUTERNAME"); // Computername
+            lblusername.Text = System.DirectoryServices.AccountManagement.UserPrincipal.Current.UserPrincipalName; // Username
+            lblipaddress.Text = GetLocalIPAddress();
+            lblmac.Text = GetMACAddress();
+            lblsno.Text = getosname();
+            lblmotherno.Text = getmothername();
+            lblramsize.Text = getramsize();
+            lblofficeversion.Text = getofficeversion();
+        }
+
+
+        public string GetMACAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String sMacAddress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (sMacAddress == String.Empty)// only return MAC Address from first card
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    sMacAddress = adapter.GetPhysicalAddress().ToString();
+                }
+            } return sMacAddress;
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = System.Net.Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public static string getosname()
+        {
+            string result = string.Empty;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                result = os["Caption"].ToString();
+                break;
+            }
+            return result;
+        }
+
+        public static string getmothername()
+        {
+            string result2 = "";
+            string result0 = "";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Product, SerialNumber,Manufacturer FROM Win32_BaseBoard");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                result2 = os["Product"].ToString();
+                result0 = os["Manufacturer"].ToString();
+                break;
+            }
+            return result0 + "-" + result2;
+        }
+
+        public static string getramsize()
+        {
+            string ramsize = "";
+            string Query = "SELECT Capacity FROM Win32_PhysicalMemory";
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(Query);
+
+            UInt64 Capacity = 0;
+            foreach (ManagementObject WniPART in searcher.Get())
+            {
+                Capacity += (Convert.ToUInt64(WniPART.Properties["Capacity"].Value) / 1048576 / 1024);
+                ramsize = Capacity.ToString() + " GB";
+            }
+
+            return ramsize;
+        }
+
+        public static string getofficeversion()
+        {
+            string sVersion = string.Empty;
+            Microsoft.Office.Interop.Word.Application appVersion = new Microsoft.Office.Interop.Word.Application();
+            appVersion.Visible = false;
+            switch (appVersion.Version.ToString())
+            {
+                case "7.0":
+                    sVersion = "95";
+                    break;
+                case "8.0":
+                    sVersion = "97";
+                    break;
+                case "9.0":
+                    sVersion = "2000";
+                    break;
+                case "10.0":
+                    sVersion = "2002";
+                    break;
+                case "11.0":
+                    sVersion = "2003";
+                    break;
+                case "12.0":
+                    sVersion = "2007";
+                    break;
+                case "14.0":
+                    sVersion = "2010";
+                    break;
+                case "15.0":
+                    sVersion = "2013";
+                    break;
+                case "16.0":
+                    sVersion = "2016 OR Above";
+                    break;
+                default:
+                    sVersion = "OOPS";
+                    break;
+            }
+            return sVersion;
+        }
+
+
+    }
+}
